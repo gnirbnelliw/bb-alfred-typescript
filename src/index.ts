09013b3b7fa371211e3ffff4b1f70460a32b04ff
Ben Willenbring <ben@onebrief.com>
2025-11-10 15:39:03 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,55 @@
-import { alfredMenuItemsSchema } from './schemas/alfred-menu-item';
+import path from 'path';
+import type { z } from 'zod';
+import { alfredMenuItemsSchema, type alfredVariableSchema } from './schemas/alfred-menu-item';
+import { alfredMenuItemFromLink } from './utils/menuUtils';
+
+const getVariables = () => {
+  const variables: z.infer<typeof alfredVariableSchema> = {
+    creator: 'Ben Willenbring',
+  };
+  return variables;
+};
 
 try {
   // Generate dynamic menu items based on query
   const menuItems = alfredMenuItemsSchema.parse({
     items: [
+      alfredMenuItemFromLink({
+        link: 'https://nodejs.org',
+        text: 'Node.js Homepage',
+        iconPath: 'dist/img/icons/node.png',
+      }),
       {
-        uid: 'sample-1',
-        title: 'Sample menu item...',
-        subtitle: 'Sample menu item',
-        arg: 'Sample',
-        autocomplete: 'Sample',
+        uid: 'get-node-version',
+        title: 'Prints the node version',
+        subtitle: 'Uses node -v',
+        arg: 'eval:node -v',
+        autocomplete: 'eval:node -v',
+        icon: { path: 'dist/img/foo.png' },
+      },
+      {
+        uid: 'run-terminal-command-1',
+        title: 'Runs terminal command: ls -la',
+        subtitle: 'Example: ls -la',
+        arg: 'terminal:ls -la',
+        autocomplete: 'terminal:ls -la',
+      },
+      {
+        uid: 'run-terminal-command-2',
+        title: 'Runs terminal command: pwd',
+        subtitle: 'Example: pwd',
+        arg: 'terminal:pwd',
+        autocomplete: 'terminal:pwd',
+      },
+      {
+        uid: 'display-markdown-sample',
+        title: 'Displays a sample markdown',
+        subtitle: 'Shows how markdown rendering works',
+        arg: 'markdown:markdown/sample.md',
+        autocomplete: 'markdown: Sample Markdown',
       },
     ],
+    variables: getVariables(),
   });
 
   // Output JSON for Alfred
