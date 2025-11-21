e9b2b3fecc43747bec3f550d2c10c5f4a02507bc
Benjamin Willenbring <ben@onebrief.com>
2025-11-21 05:56:11 -0500

    fix: make icons resolve correctly in alfred

--- src/schemas/alfred-icon.ts
+++ src/schemas/alfred-icon.ts
@@ -24,7 +24,7 @@ export const normalizedIconPath = (iconPath: string): string => {
   try {
     // Normalize the path
     const fileName = path.basename(iconPath);
-    const normalizedPath = `dist/img/icons/${fileName}`;
+    const normalizedPath = path.join(iconFolderPath, fileName);
     return normalizedPath;
   } catch (e) {
     console.error('⚠️ Error normalizing icon path:', iconPath);
@@ -36,18 +36,21 @@ export const normalizedIconPath = (iconPath: string): string => {
 export const alfredIconSchema = z.object({
   path: z.string().transform((val) => {
     // if the icon exists, use it. Otherwise, use default app icon.
-    return iconFileExists(val) ? val : defaultAppIconPath();
+    return iconFileExists(val) ? normalizedIconPath(val) : defaultAppIconPath();
   }),
 });
 
 // Typescript type inferred from the schema
 export type AlfredIcon = z.infer<typeof alfredIconSchema>;
 
+const iconPath = normalizedIconPath('emoji.png');
 /**
  * The default path to alfred icon.
  * @returns { string }
  */
-export const defaultAppIconPath = (): string => 'dist/img/icons/alfred.png';
+export const defaultAppIconPath = (): string => {
+  return normalizedIconPath('alfred.png');
+};
 
 export const defaultAppIcon = (): AlfredIcon => {
   return alfredIconSchema.parse({
@@ -55,4 +58,3 @@ export const defaultAppIcon = (): AlfredIcon => {
     path: defaultAppIconPath(),
   });
 };
-
